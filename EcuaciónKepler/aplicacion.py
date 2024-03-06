56fa4a2593295d9639ceff05f298f56644c364ad
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-03-06 00:39:12 -0600

    py iniciales para los métodos a utilzar en kepler

---
+++ EcuaciónKepler/aplicacion.py
@@ -0,0 +1,9 @@
+'''
+En está sección se desarrollará la ecuación de kepler utilizando los metodos de Jarrat y Potrak-Ptak
+E = es la anomalía excéntrica
+e = es la excentricidad de la elipse
+M = es la anomalía media
+'''
+Ecuacion = "M = E - e*sin(E)"
+
+print(Ecuacion)
